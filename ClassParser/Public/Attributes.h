53f013a5dee62add03a069b2fdb259c24bfed295
Roman Chehowski <roman.chehowski@vizor-games.com>
2021-01-01 13:12:00 +0100

    A bit of extra tabs

--- ClassParser/Public/Attributes.h
+++ ClassParser/Public/Attributes.h
@@ -55,26 +55,26 @@ namespace Parser
     class CAttributeNames
     {
     public:
-        DEFINE_ATTRIBUTE_NAME(0,    ConstantValue, 45, 3);
-        DEFINE_ATTRIBUTE_NAME(1,    Code, 45, 3);
-        DEFINE_ATTRIBUTE_NAME(2,    StackMapTable, 50, 0);
-        DEFINE_ATTRIBUTE_NAME(3,    Exceptions, 45, 3);
-        DEFINE_ATTRIBUTE_NAME(4,    InnerClasses, 45, 3);
-        DEFINE_ATTRIBUTE_NAME(5,    EnclosingMethod, 49, 0);
-        DEFINE_ATTRIBUTE_NAME(6,    Synthetic, 45, 3);
-        DEFINE_ATTRIBUTE_NAME(7,    Signature, 49, 0);
-        DEFINE_ATTRIBUTE_NAME(8,    SourceFile, 45, 3);
-        DEFINE_ATTRIBUTE_NAME(9,    SourceDebugExtension, 49, 0);
-        DEFINE_ATTRIBUTE_NAME(10,   LineNumberTable, 45, 3);
-        DEFINE_ATTRIBUTE_NAME(11,   LocalVariableTable, 45, 3);
-        DEFINE_ATTRIBUTE_NAME(12,   LocalVariableTypeTable, 49, 0);
-        DEFINE_ATTRIBUTE_NAME(13,   Deprecated, 45, 3);
-        DEFINE_ATTRIBUTE_NAME(14,   RuntimeVisibleAnnotations, 49, 0);
-        DEFINE_ATTRIBUTE_NAME(15,   RuntimeInvisibleAnnotations, 49, 0);
-        DEFINE_ATTRIBUTE_NAME(16,   RuntimeVisibleParameterAnnotations, 49, 0);
-        DEFINE_ATTRIBUTE_NAME(17,   RuntimeInvisibleParameterAnnotations, 49, 0);
-        DEFINE_ATTRIBUTE_NAME(18,   AnnotationDefault, 49, 0);
-        DEFINE_ATTRIBUTE_NAME(19,   BootstrapMethods, 51, 0);
+        DEFINE_ATTRIBUTE_NAME(0,    ConstantValue,                          45, 3);
+        DEFINE_ATTRIBUTE_NAME(1,    Code,                                   45, 3);
+        DEFINE_ATTRIBUTE_NAME(2,    StackMapTable,                          50, 0);
+        DEFINE_ATTRIBUTE_NAME(3,    Exceptions,                             45, 3);
+        DEFINE_ATTRIBUTE_NAME(4,    InnerClasses,                           45, 3);
+        DEFINE_ATTRIBUTE_NAME(5,    EnclosingMethod,                        49, 0);
+        DEFINE_ATTRIBUTE_NAME(6,    Synthetic,                              45, 3);
+        DEFINE_ATTRIBUTE_NAME(7,    Signature,                              49, 0);
+        DEFINE_ATTRIBUTE_NAME(8,    SourceFile,                             45, 3);
+        DEFINE_ATTRIBUTE_NAME(9,    SourceDebugExtension,                   49, 0);
+        DEFINE_ATTRIBUTE_NAME(10,   LineNumberTable,                        45, 3);
+        DEFINE_ATTRIBUTE_NAME(11,   LocalVariableTable,                     45, 3);
+        DEFINE_ATTRIBUTE_NAME(12,   LocalVariableTypeTable,                 49, 0);
+        DEFINE_ATTRIBUTE_NAME(13,   Deprecated,                             45, 3);
+        DEFINE_ATTRIBUTE_NAME(14,   RuntimeVisibleAnnotations,              49, 0);
+        DEFINE_ATTRIBUTE_NAME(15,   RuntimeInvisibleAnnotations,            49, 0);
+        DEFINE_ATTRIBUTE_NAME(16,   RuntimeVisibleParameterAnnotations,     49, 0);
+        DEFINE_ATTRIBUTE_NAME(17,   RuntimeInvisibleParameterAnnotations,   49, 0);
+        DEFINE_ATTRIBUTE_NAME(18,   AnnotationDefault,                      49,0);
+        DEFINE_ATTRIBUTE_NAME(19,   BootstrapMethods,                       51, 0);
 
         static constexpr size_t GetNumAttributeNames();
 
