e7cc0f4c19146234580370b360c39d5212bf0ee2
Roman Chehowski <roman.chehowski@vizor-games.com>
2021-02-03 23:50:51 +0100

    Minor refactoring

--- Source/Compiler/Model/Private/ConstantPool/ConstantInfo.cpp
+++ Source/Compiler/Model/Private/ConstantPool/ConstantInfo.cpp
@@ -37,23 +37,6 @@ namespace Parse
     using CConstantInfoSpawnFunction = std::function<std::shared_ptr<CConstantInfo>(EConstantPoolInfoTag)>;
     using CTagToConstantInfoSpawnFunction = std::unordered_map<EConstantPoolInfoTag, CConstantInfoSpawnFunction>;
 
-    const CTagToConstantInfoSpawnFunction G_TagToConstantInfoSpawnFunction {
-        ADD_CONSTANT_INFO(Utf8),
-        ADD_CONSTANT_INFO(Integer),
-        ADD_CONSTANT_INFO(Float),
-        ADD_CONSTANT_INFO(Long),
-        ADD_CONSTANT_INFO(Double),
-        ADD_CONSTANT_INFO(Class),
-        ADD_CONSTANT_INFO(String),
-        ADD_CONSTANT_INFO(FieldRef),
-        ADD_CONSTANT_INFO(MethodRef),
-        ADD_CONSTANT_INFO(InterfaceMethodRef),
-        ADD_CONSTANT_INFO(NameAndType),
-        ADD_CONSTANT_INFO(MethodHandle),
-        ADD_CONSTANT_INFO(MethodType),
-        ADD_CONSTANT_INFO(InvokeDynamic),
-    };
-
     void operator>>(CClassReader &Reader, CConstantInfo &Instance)
     {
         Instance.DeserializeFrom(Reader);
@@ -61,6 +44,23 @@ namespace Parse
 
     std::shared_ptr<CConstantInfo> CConstantInfo::NewConstantInfo(EConstantPoolInfoTag ConstantPoolInfoTag)
     {
+        static const CTagToConstantInfoSpawnFunction G_TagToConstantInfoSpawnFunction {
+            ADD_CONSTANT_INFO(Utf8),
+            ADD_CONSTANT_INFO(Long),
+            ADD_CONSTANT_INFO(Float),
+            ADD_CONSTANT_INFO(Class),
+            ADD_CONSTANT_INFO(Double),
+            ADD_CONSTANT_INFO(String),
+            ADD_CONSTANT_INFO(Integer),
+            ADD_CONSTANT_INFO(FieldRef),
+            ADD_CONSTANT_INFO(MethodRef),
+            ADD_CONSTANT_INFO(MethodType),
+            ADD_CONSTANT_INFO(NameAndType),
+            ADD_CONSTANT_INFO(MethodHandle),
+            ADD_CONSTANT_INFO(InvokeDynamic),
+            ADD_CONSTANT_INFO(InterfaceMethodRef),
+        };
+
         auto It = G_TagToConstantInfoSpawnFunction.find(ConstantPoolInfoTag);
 
         ASSERT_MSG(It != G_TagToConstantInfoSpawnFunction.end(),
